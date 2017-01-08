efcb06396121dd8d3b8d11ca68ba03cdbd3e66df
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-01-09 06:31:44 +0700

    Add hello-world example

---
+++ examples/hello-world.js
@@ -0,0 +1,21 @@
+var amqp = require('../index');
+var uri = 'amqp://krcihysa:j-_iaJkdSKNHOK7SjsVXLAZG6hzHG5kL@hyena.rmq.cloudamqp.com/krcihysa';
+//------------------------------------------------------------------------
+amqp.initialize(uri); // Optional callback
+
+//------------------------------------------------------------------------
+// Receiver side
+var queue = new amqp.Queue({ name: 'my-first-queue', assertOpts: { durable: true }, deleteOpts: {} });
+var consumer = new amqp.Consumer(queue, { opts: { noAck: true } });
+
+consumer.consume();
+consumer.on('message', (msg) => {
+    console.log('Message received:', msg);
+    consumer.cancel();
+});
+
+
+//------------------------------------------------------------------------
+// Provider side
+var provider = new amqp.Provider();
+provider.sendToQueue(queue, 'Ex1MSG', (e) => { console.log('ex1 published', e); });
